af4567bb2a5adb1817c8047bfb2f253fc38703ab
matheusmppaiva <matheus.paiva@gec.inatel.br>
2022-09-29 23:40:14 -0300

    Alteração de tela de login e inclusão de nova tela

---
+++ app/src/main/java/com/example/jogoeducando/jogoABC.java
@@ -0,0 +1,14 @@
+package com.example.jogoeducando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jogoABC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_abc);
+    }
+}
