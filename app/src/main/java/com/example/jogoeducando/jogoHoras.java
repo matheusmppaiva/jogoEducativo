4eb40a786adf090a106fc3c6f84770ebdc141dc6
matheusmppaiva <matheus.paiva@gec.inatel.br>
2022-10-13 21:29:21 -0300

    Criacao e atualizacao de telas

---
+++ app/src/main/java/com/example/jogoeducando/jogoHoras.java
@@ -0,0 +1,14 @@
+package com.example.jogoeducando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jogoHoras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_horas);
+    }
+}
